2d30f2d29bf2548a6809b68dd346d486b25c9a68
KiwiG01 <nicoagusserranoborquez@hotmail.com>
2024-09-15 15:33:30 -0300

    added supposed networking base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ursina import *
+from ursina.networking import *
 
 app = Ursina(title="fire in da hole", icon="fith.ico")
 
@@ -7,5 +8,24 @@ window.exit_button.enable()
 window.fps_counter.disable()
 window.entity_counter.disable()
 window.collider_counter.disable()
+
+rpcpeer = RPCPeer()
+
+@rpc(rpcpeer)
+def on_connect(cn, time):
+    print("connection added or smh")
+
+@rpc(rpcpeer)
+def on_disconnect(cn, time):
+    print("connection removed or smh")
+
+@rpc(rpcpeer)
+def say(cn, time, txt):
+    print(txt)
+
+def update():
+    rpcpeer.update()
+    if 
+    
 
 app.run()
