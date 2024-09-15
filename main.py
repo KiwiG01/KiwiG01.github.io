9820e933703ae2e02cf41d62efdbf7613dbebf7e
KiwiG01 <nicoagusserranoborquez@hotmail.com>
2024-09-15 15:39:01 -0300

    fire in da hole button!!1

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ window.collider_counter.disable()
 
 rpcpeer = RPCPeer()
 
+fith = Audio(sound_file_name="fire-in-the-hole.mp3", autoplay=False)
+
+click = Button(icon="button.png", scale=(0.4,0.4))
+click.on_click = fith.play
+
 @rpc(rpcpeer)
 def on_connect(cn, time):
     print("connection added or smh")
@@ -20,12 +25,11 @@ def on_disconnect(cn, time):
     print("connection removed or smh")
 
 @rpc(rpcpeer)
-def say(cn, time, txt):
+def say(cn, time, txt:str):
     print(txt)
 
 def update():
     rpcpeer.update()
-    if 
     
 
 app.run()
