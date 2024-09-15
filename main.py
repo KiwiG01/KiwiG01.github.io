4cac649d0bce6d3b0610e06319f1528a3f110dd7
KiwiG01 <nicoagusserranoborquez@hotmail.com>
2024-09-15 15:25:00 -0300

    cleaned up ui

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from ursina import *
 
 app = Ursina(title="fire in da hole", icon="fith.ico")
 
+window.exit_button.disable()
+window.fps_counter.disable()
+window.entity_counter.disable()
+window.collider_counter.disable()
+
 app.run()
