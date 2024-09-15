6de5b0d1c470d08662760165f864513851e2d16e
KiwiG01 <nicoagusserranoborquez@hotmail.com>
2024-09-15 15:27:05 -0300

    fixed wierd error

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ursina import *
 app = Ursina(title="fire in da hole", icon="fith.ico")
 
 window.borderless = True
+window.exit_button.enable()
 window.fps_counter.disable()
 window.entity_counter.disable()
 window.collider_counter.disable()
